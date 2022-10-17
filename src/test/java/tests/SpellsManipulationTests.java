2f3be05880c13af7d77f33d1157b1d9a47334df8
teddu0 <107142125+teddu0@users.noreply.github.com>
2022-10-17 23:10:27 +0300

    Подключил allure

--- src/test/java/tests/SpellsManipulationTests.java
+++ src/test/java/tests/SpellsManipulationTests.java
@@ -1,37 +1,51 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
 import org.testng.annotations.Test;
 
 
 public class SpellsManipulationTests extends TestBase {
     RestHelper rh = new RestHelper();
 
-    @Test (description = "Получение всего списка заклинаний без параметров")
+    @Test
+    @Owner("Константин В.")
+    @Description("Получение всего списка заклинаний без параметров")
     public void testGettingAllSpells(){
         rh.getAllSpells();
     }
 
-    @Test (description = "Получение заклинания по существующему имени")
+    @Test
+    @Owner("Константин В.")
+    @Description("Получение заклинания по существующему имени")
     public void testGettingSpellByExistingNameOfSpell(){
         rh.getSpellsByParameters("name", "Alarte Ascendare");
     }
 
-    @Test (description = "Получение заклиная по существующему типу")
+    @Test
+    @Owner("Константин В.")
+    @Description("Получение заклиная по существующему типу")
     public void testGettingSpellByExistingTypeOfSpell(){
         rh.getSpellsByParameters("type", "Charm");
     }
 
-    @Test (description = "Получение заклиная по существующему заклинанию")
+    @Test
+    @Owner("Константин В.")
+    @Description("Получение заклиная по существующему заклинанию")
     public void testGettingSpellByExistingSpell(){
         rh.getSpellsByParameters("incantation", "Aparecium");
     }
 
-    @Test (description = "Получение заклиная по существующему ID")
+    @Test
+    @Owner("Константин В.")
+    @Description("Получение заклиная по существующему ID")
     public void testGettingSpellByExistingId(){
         rh.getSpellByExsistingId(EXISTING_ID_OF_SPELL);
     }
 
-    @Test (description = "Получение заклиная по несуществующему ID")
+    @Test
+    @Owner("Константин В.")
+    @Description("Получение заклиная по несуществующему ID")
     public void testGettingSpellByNonExistingId(){
         rh.getSpellByNonExsistingId(NON_EXISTING_ID_OF_SPELL);
     }
