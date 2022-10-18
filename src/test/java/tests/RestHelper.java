b26657621dcbe7800ecbf110fc99d2339841bfdd
teddu0 <107142125+teddu0@users.noreply.github.com>
2022-10-18 17:28:26 +0300

    Итоговый вариант

--- src/test/java/tests/RestHelper.java
+++ src/test/java/tests/RestHelper.java
@@ -1,46 +1,55 @@
 package tests;
 
-import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
 
 public class RestHelper {
-    String uri = "https://wizard-world-api.herokuapp.com";
-    public int getAllSpells(){
-        RestAssured.baseURI = uri;
+
+    private final String URI = "https://wizard-world-api.herokuapp.com";
+    private final String SPELLS_PATH = "/Spells";
+    private final String VALID_SPELL_PATH = SPELLS_PATH + "/6bed818d-1006-4b52-9d64-e8738077fc1f";
+    private final String INVALID_SPELL_PATH = SPELLS_PATH + "/99170271-1b6a-4c10-a947-37ee558b6c83";
+
+    RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setBaseUri(URI)
+            .setAccept(ContentType.JSON)
+            .build();
+
+    public int getAllSpells() {
         Response response = given()
+                .spec(requestSpecification)
                 .when()
-                .get("/Spells");
+                .get(SPELLS_PATH);
         return response.getStatusCode();
     }
 
-    public String getSpellsByParameters(String key, String value){
-        RestAssured.baseURI = uri;
+    public String getSpellsByParameters(String key, String value) {
         Response response = given()
+                .spec(requestSpecification)
                 .when()
                 .param(key, value)
-                .get("/Spells");
-        return response.jsonPath().getString("[0]."+key);
+                .get(SPELLS_PATH);
+        return response.jsonPath().getString("[0]." + key);
     }
 
-
-    public int getSpellByExsistingId(String id){
-        RestAssured.baseURI = uri;
+    public int getSpellByExistingId() {
         Response response = given()
+                .spec(requestSpecification)
                 .when()
-                .get("/Spells/"+id);
+                .get(VALID_SPELL_PATH);
         return response.getStatusCode();
 
     }
 
-    public int getSpellByNonExsistingId(String id){
-        RestAssured.baseURI = uri;
+    public int getSpellByNonExistingId() {
         Response response = given()
+                .spec(requestSpecification)
                 .when()
-                .get("/Spells/"+id);
+                .get(INVALID_SPELL_PATH);
         return response.getStatusCode();
     }
 
--- src/test/java/tests/SpellsManipulationTests.java
+++ src/test/java/tests/SpellsManipulationTests.java
@@ -1,67 +1,53 @@
 package tests;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.qameta.allure.Description;
 import io.qameta.allure.Owner;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-
-
-public class SpellsManipulationTests extends TestBase {
+public class SpellsManipulationTests {
     RestHelper rh = new RestHelper();
 
     @Test
     @Owner("Константин В.")
     @Description("Получение всего списка заклинаний без параметров")
-    public void testGettingAllSpells(){
-        rh.getAllSpells();
+    public void testGettingAllSpells() {
         Assert.assertEquals(200, rh.getAllSpells());
     }
 
     @Test
     @Owner("Константин В.")
     @Description("Получение заклинания по существующему имени")
-    public void testGettingSpellByExistingNameOfSpell(){
-        rh.getSpellsByParameters("name", "Alarte Ascendare");
+    public void testGettingSpellByExistingNameOfSpell() {
         Assert.assertEquals("Alarte Ascendare", rh.getSpellsByParameters("name", "Alarte Ascendare"));
     }
 
     @Test
     @Owner("Константин В.")
     @Description("Получение заклиная по существующему типу")
-    public void testGettingSpellByExistingTypeOfSpell(){
-        rh.getSpellsByParameters("type", "Charm");
+    public void testGettingSpellByExistingTypeOfSpell() {
         Assert.assertEquals("Charm", rh.getSpellsByParameters("type", "Charm"));
     }
 
     @Test
     @Owner("Константин В.")
     @Description("Получение заклиная по существующему заклинанию")
-    public void testGettingSpellByExistingSpell(){
-        rh.getSpellsByParameters("incantation", "Aparecium");
+    public void testGettingSpellByExistingSpell() {
         Assert.assertEquals("Aparecium", rh.getSpellsByParameters("incantation", "Aparecium"));
     }
 
     @Test
     @Owner("Константин В.")
     @Description("Получение заклиная по существующему ID")
-    public void testGettingSpellByExistingId(){
-        rh.getSpellByExsistingId(EXISTING_ID_OF_SPELL);
-        Assert.assertEquals(200, rh.getSpellByExsistingId(EXISTING_ID_OF_SPELL));
+    public void testGettingSpellByExistingId() {
+        Assert.assertEquals(200, rh.getSpellByExistingId());
     }
 
     @Test
     @Owner("Константин В.")
     @Description("Получение заклиная по несуществующему ID")
-    public void testGettingSpellByNonExistingId(){
-        rh.getSpellByNonExsistingId(NON_EXISTING_ID_OF_SPELL);
-        Assert.assertEquals(404, rh.getSpellByNonExsistingId(NON_EXISTING_ID_OF_SPELL));
+    public void testGettingSpellByNonExistingId() {
+        Assert.assertEquals(404, rh.getSpellByNonExistingId());
     }
 }
